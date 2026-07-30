0987704a4303a9c27ed1122bb0870dd7222a020d
baremetal245 <aditya24g@protonmail.com>
2026-07-30 18:14:13 +0530

    feat: initial commit with python web app

---
+++ app.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 8080
+
+class SimpleHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>DevOps Step 1: Hello World from Linux Mint!</h1>")
+
+if __name__ == "__main__":
+    with socketserver.TCPServer(("", PORT), SimpleHandler) as httpd:
+        print(f"Server running on port {PORT}...")
+        httpd.serve_forever()
