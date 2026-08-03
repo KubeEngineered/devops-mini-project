737558c809a13240fb5ac42f7ffe20d57a8694d9
baremetal245 <aditya24g@protonmail.com>
2026-08-03 20:19:29 +0530

    feature work done

---
+++ sourcecode.py
@@ -0,0 +1,18 @@
+
+# This file does simple mathematical ops using arithematic operators
+
+
+a = 1
+b = 10
+result = a + b
+print(f"{result} is the output of the operation")
+
+
+
+x = 11
+y = 27
+z = 11 * 27
+print("The output of the operation is z")
+
+# End of file
+
